7eef5e965246f1ab9001ff2be0fd66ba7b2d43e8
Keith Wygant <keith.wygant@gmail.com>
2016-12-17 21:01:22 -0500

    enabled brightness controls.

--- xmonad.hs
+++ xmonad.hs
@@ -3,15 +3,28 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Layout.NoBorders
+import XMonad.Util.EZConfig (additionalKeys)
 import System.IO (hPutStrLn)
+import Graphics.X11.ExtraTypes.XF86
+
+myModKey = mod4Mask -- Win key
+myOtherKeys = [
+        ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 2%+"),
+        ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 2%-"),
+        ((0, xF86XK_AudioMute), spawn "amixer -D pulse set Master toggle"),
+        ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 8"),
+        ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 8"),
+        ((0, xF86XK_KbdBrightnessUp), spawn "/Ix/keith/Settings/xmonad/kbd-backlight.sh up"),
+        ((0, xF86XK_KbdBrightnessDown), spawn "/Ix/keith/Settings/xmonad/kbd-backlight.sh down")
+        ]
 
 main = do
   xmproc <- spawnPipe "xmobar /Ix/keith/Settings/xmonad/xmobarrc"
-  xmonad defaultConfig {
+  xmonad (defaultConfig {
     borderWidth = 1,
     normalBorderColor = "black",
     focusedBorderColor = "#ff8100",
-    modMask = mod4Mask, -- 'Win' key
+    modMask = myModKey,
     terminal = "cool-retro-term",
     manageHook = manageDocks <+> manageHook defaultConfig,
     layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig,
@@ -20,4 +33,4 @@ main = do
         ppOutput = hPutStrLn xmproc,
         ppTitle = shorten 50
         }
-    }
+    } `additionalKeys` myOtherKeys)
