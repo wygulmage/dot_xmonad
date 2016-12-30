9a0b75ca089cc6ca94d1dacf0043e6bdd26c5af0
Keith Wygant <keith.wygant@gmail.com>
2016-12-30 12:53:51 -0500

    reformatted xmonad.hs and added Stackset import.

--- xmonad.hs
+++ xmonad.hs
@@ -4,23 +4,24 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig (additionalKeys)
+import qualified XMonad.StackSet as W -- for window management commands.
 import System.IO (hPutStrLn)
 import Graphics.X11.ExtraTypes.XF86
 
 myModKey = mod4Mask -- Win key
-myOtherKeys = [
-        ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 2%+"),
-        ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 2%-"),
-        ((0, xF86XK_AudioMute), spawn "amixer -D pulse set Master toggle"),
-        ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 8"),
-        ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 8"),
-        ((0, xF86XK_KbdBrightnessUp), spawn "/Ix/keith/Settings/xmonad/kbd-backlight.sh up"),
-        ((0, xF86XK_KbdBrightnessDown), spawn "/Ix/keith/Settings/xmonad/kbd-backlight.sh down")
-        ]
+myOtherKeys =
+        ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 2%+") :
+        ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 2%-") :
+        ((0, xF86XK_AudioMute), spawn "amixer -D pulse set Master toggle") :
+        ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 8") :
+        ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 8") :
+        ((0, xF86XK_KbdBrightnessUp), spawn "/Ix/keith/Settings/xmonad/kbd-backlight.sh up") :
+        ((0, xF86XK_KbdBrightnessDown), spawn "/Ix/keith/Settings/xmonad/kbd-backlight.sh down") :
+        []
 
 main = do
   xmproc <- spawnPipe "xmobar /Ix/keith/Settings/xmonad/xmobarrc"
-  xmonad (defaultConfig {
+  xmonad $ defaultConfig {
     borderWidth = 1,
     normalBorderColor = "black",
     focusedBorderColor = "#ff8100",
@@ -33,4 +34,4 @@ main = do
         ppOutput = hPutStrLn xmproc,
         ppTitle = shorten 50
         }
-    } `additionalKeys` myOtherKeys)
+    } `additionalKeys` myOtherKeys
