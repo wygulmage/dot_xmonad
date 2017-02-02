b15aa1bcaf4150fd6db152e77790cf37eda64f3d
Keith Wygant <keith.wygant@gmail.com>
2017-02-01 21:36:27 -0500

    switched to use Ix.
    I also switched from using XMonad defaultConfig ('depreciated') to
    XMonad.Config def. And I cut down the number of layouts and added a hook
    for smplayer.

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
+import XMonad.Config (def)
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers (doFullFloat)
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig (additionalKeys)
@@ -16,24 +18,25 @@ myOtherKeys =
         ((0, xF86XK_AudioMute), spawn "amixer -D pulse set Master toggle") :
         ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 8") :
         ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 8") :
-        ((0, xF86XK_KbdBrightnessUp), spawn "/Ix/keith/Settings/xmonad/kbd-backlight.sh up") :
-        ((0, xF86XK_KbdBrightnessDown), spawn "/Ix/keith/Settings/xmonad/kbd-backlight.sh down") :
+        ((0, xF86XK_KbdBrightnessUp), spawn "/Ix/k/Settings/xmonad/kbd-backlight.sh up") :
+        ((0, xF86XK_KbdBrightnessDown), spawn "/Ix/k/Settings/xmonad/kbd-backlight.sh down") :
         []
 
 main = do
-  xmproc <- spawnPipe "xmobar /Ix/keith/Settings/xmonad/xmobarrc"
-  xmonad $ defaultConfig {
+  xmproc <- spawnPipe "xmobar /Ix/k/Settings/xmonad/xmobarrc"
+  xmonad $ def {
     borderWidth = 1,
     normalBorderColor = "black",
     focusedBorderColor = "#ff8100",
     modMask = myModKey,
     terminal = myTerminal,
-    manageHook = manageDocks <+> manageHook defaultConfig,
-    layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig,
-    handleEventHook = docksEventHook `mappend` handleEventHook defaultConfig,
+    manageHook = manageDocks <+> (className =? "smplayer" --> doFullFloat) <+> manageHook def, -- to make this work properly I need another Query that gets the current layout and compares it to "Full". Hints: liftX, ask, description . W.layout . W.workspace . W.current.
+    layoutHook = avoidStruts $ smartBorders $ (Tall 1 (3/100) (1/2)) ||| Full,
+    handleEventHook = docksEventHook `mappend` handleEventHook def,
     logHook = dynamicLogWithPP xmobarPP {
         ppOutput = hPutStrLn xmproc,
         ppTitle = shorten 50
         },
-    startupHook = (spawn myTerminal) <+> startupHook defaultConfig
+    startupHook = (spawn myTerminal) <+> startupHook def
     } `additionalKeys` myOtherKeys
+
