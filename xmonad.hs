b83c3d57132dca28ab10dfb0d8fa9e94485084e1
Keith <wygulmage@users.noreply.github.com>
2019-10-30 20:18:51 -0400

    organized imports.

--- xmonad.hs
+++ xmonad.hs
@@ -19,6 +19,8 @@ import XMonad
    )
 import XMonad.Core (Query(..), WindowSet(..))
 import XMonad.Config (def)
+import qualified XMonad.StackSet as W -- for window management commands.
+
 import XMonad.Actions.UpdatePointer (updatePointer)
 import XMonad.Hooks.DynamicLog
    ( PP (ppLayout, ppCurrent, ppOutput, ppTitle)
@@ -28,11 +30,11 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops (ewmh)
 import XMonad.Hooks.ManageDocks (manageDocks, avoidStruts, docksEventHook)
 import XMonad.Hooks.ManageHelpers (doFullFloat, isFullscreen)
-import XMonad.Util.Run (safeSpawn, unsafeSpawn, spawnPipe)
-import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Layout.Fullscreen (fullscreenSupport, fullscreenManageHook, fullscreenEventHook)
+import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Util.EZConfig (additionalKeys)
-import qualified XMonad.StackSet as W -- for window management commands.
+import XMonad.Util.Run (safeSpawn, unsafeSpawn, spawnPipe)
+
 import System.IO (hPutStrLn)
 import Graphics.X11.ExtraTypes.XF86 -- for the mOtherKeys KeySyms.
 -- import Data.Semigroup ((<>))
@@ -54,8 +56,10 @@ myOtherKeys =
         ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 2%+") :
         ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 2%-") :
         ((0, xF86XK_AudioMute), spawn "amixer -D pulse set Master toggle") :
-        ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +10 -time 0 -steps 1") :
-        ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -10 -time 0 -steps 1") :
+        ((0, xF86XK_MonBrightnessUp), spawn "light -A 5") :
+        ((0, xF86XK_MonBrightnessDown), spawn "light -U 5") :
+        -- ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +10 -time 0 -steps 1") :
+        -- ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -10 -time 0 -steps 1") :
         ((0, xF86XK_KbdBrightnessUp), spawn "/Ix/k/Settings/xmonad/kbd-backlight.sh up") :
         ((0, xF86XK_KbdBrightnessDown), spawn "/Ix/k/Settings/xmonad/kbd-backlight.sh down") :
         []
